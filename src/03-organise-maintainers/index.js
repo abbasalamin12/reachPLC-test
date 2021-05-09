c10968e60a095493e8f7386df3d3de8375440ce3
amina15 <amina15@coventry.ac.uk>
2021-05-09 13:38:25 +0100

    completed the last task

--- src/03-organise-maintainers/index.js
+++ src/03-organise-maintainers/index.js
@@ -41,40 +41,66 @@ The results should have this structure:
  * be in alphabetical order.
  */
 
-axios
-  .post('http://ambush-api.inyourarea.co.uk/ambush/intercept', {
-    url: 'https://api.npms.io/v2/search/suggestions?q=react',
-    method: 'GET',
-    return_payload: true,
-  })
-  .then(function (response) {
-    dataContent = response.data.content;
-
-    let maintainerArray = [];
-
-    dataContent.forEach(element => {
-      let packageMaintainers = element.package.maintainers;
-      packageMaintainers.forEach(element => {
-        if (!maintainerArray.includes(element.username)) {
-          maintainerArray.push(element.username);
-        }
-      });
-    });
-
-    maintainerArray = maintainerArray.map(maintainer => {
-      return {
-        username: maintainer,
-        packageNames: [],
-      };
-    });
-    console.log(maintainerArray);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
 module.exports = async function organiseMaintainers() {
-  // TODO
+  return axios
+    .post('http://ambush-api.inyourarea.co.uk/ambush/intercept', {
+      url: 'https://api.npms.io/v2/search/suggestions?q=react',
+      method: 'GET',
+      return_payload: true,
+    })
+    .then(function (response) {
+      dataContent = response.data.content;
 
-  return maintainers;
+      let organisedMaintainers = []; // this array will contain each maintainer name and their associated packages
+
+      dataContent.forEach(element => {
+        let currentPackageMaintainers = element.package.maintainers;
+        currentPackageMaintainers.forEach(currentPackageMaintainers => {
+          // for each package, add each maintainer to an array unless it already exists
+          if (
+            organisedMaintainers.every(
+              // if it doesnt exist in the array
+              maintainer =>
+                maintainer.username != currentPackageMaintainers.username,
+            )
+          ) {
+            organisedMaintainers.push({
+              // add it to the array with its package
+              username: currentPackageMaintainers.username,
+              packageNames: [element.package.name],
+            });
+          } else {
+            // if it already exists
+            let maintainerIndex = organisedMaintainers.findIndex(
+              maintainer =>
+                maintainer.username == currentPackageMaintainers.username,
+            );
+            // then add the packages to the maintainer's list of packages
+            organisedMaintainers[maintainerIndex].packageNames.push(
+              element.package.name,
+            );
+          }
+        });
+
+        // sort things in alphabetical order
+        // sort each package list in alphabetical order
+        organisedMaintainers.forEach(organisedMaintainer =>
+          organisedMaintainer.packageNames.sort(),
+        );
+        // sort the entire array in alphabetical order
+        organisedMaintainers.sort(function (a, b) {
+          if (a.username < b.username) {
+            return -1;
+          }
+          if (a.username > b.username) {
+            return 1;
+          }
+          return 0;
+        });
+      });
+      return organisedMaintainers;
+    })
+    .catch(error => {
+      console.log(error);
+    });
 };
